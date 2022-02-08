97ed7a1145b21c241994b1c555404c20b67b7d5d
Animeska34 <59974154+Animeska34@users.noreply.github.com>
2022-02-08 13:57:55 +0200

    Classwork 20/08

---
+++ Engines/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public delegate void Test();
+        public event Test? DoTest;
+        public Action<Int32, Int32, Int32>? action;
+        public Func<Int32, Int32, String>? func;
+        public static void Main()
+        {
+            A a1 = new();
+            A a2 = new();
+            B b = new();
+            Program p = new Program();
+
+            a1.Register(p);
+            a2.Register(p);
+            b.Register(p);
+            p.DoTest?.Invoke();
+
+            p.func += p.Add;
+            Console.WriteLine(p.func?.Invoke(4, 3));
+        }
+
+        String Add(Int32 a, Int32 b)
+        {
+            return (a+b).ToString();
+        }
+    }
+
+    public class A
+    {
+        public void Register(Program p)
+        {
+            p.DoTest += Act;
+        }
+
+        public void Act()
+        {
+            Console.WriteLine("A Exec");
+        }
+    }
+
+    public class B
+    {
+        public void Register(Program p)
+        {
+            p.DoTest += Act;
+        }
+
+        public void Act()
+        {
+            Console.WriteLine("B Exec");
+        }
+    }
+}
---
+++ Engines/ConsoleApp3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Program
+    {
+        class Animal
+        {
+            virtual public void Roar()
+            {
+                Console.WriteLine("Zzzz");
+            }
+        }
+
+        class Dog : Animal
+        {
+            override public void Roar()
+            {
+                Console.WriteLine("Gav-Gav");
+            }
+        }
+        class Cat : Animal
+        {
+            override public void Roar()
+            {
+                Console.WriteLine("Meow-Meow");
+            }
+        }
+
+        class Fox : Animal
+        {
+            override public void Roar()
+            {
+                Console.WriteLine("Kon-Kon");
+            }
+        }
+        class Tanuki : Animal
+        {
+            override public void Roar()
+            {
+                Console.WriteLine("Pon-Pon");
+            }
+        }
+        class Boozer : Animal
+        {
+            override public void Roar()
+            {
+                Console.WriteLine("Blew");
+            }
+        }
+
+        public static void Main()
+        {
+            Animal[] a =
+            {
+                new(),
+                new Dog(),
+                new Cat(),
+                new Fox(),
+                new Tanuki(),
+                new Boozer()
+            };
+
+            foreach (var item in a)
+                item.Roar();
+        }
+    }
+}
