823d3dc72fc0d102f10b13d7ccc6e1f2b5e12ee4
Animeska34 <59974154+Animeska34@users.noreply.github.com>
2022-02-15 14:27:17 +0200

    Add Engines CW-03 & CW-04

---
+++ Engines/CW-03 Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CW_03
+{
+    class Program
+    {
+
+        static List<UInt64> results = new();
+        public static void Main()
+        {
+            Thread thread = new(Calc);
+            thread.Start();
+            while (thread.IsAlive || results.Count > 0)
+            {
+                if (results.Count > 0)
+                {
+                    Console.WriteLine(results[0]);
+                    results.RemoveAt(0);
+                }
+            }
+        }
+
+        static void Calc()
+        {
+            UInt64 lir = 1;
+            for (UInt64 i = 1; i < 50; i++)
+            {
+                lir *= i;
+                results.Add(lir);
+            }
+        }
+    }
+}
---
+++ Engines/CW-04 ref, struct/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CW_04
+{
+    class Program : IDisposable
+    {
+        struct Test
+        {
+            public int x;
+            public int y;
+        }
+        public static void Main()
+        {
+            var a = 10;
+            var b = 10;
+            var rtest = new Test() { x = a, y = b };
+            var test = rtest;
+            var outVar = 5;
+            TryRefTest(a, ref b, ref rtest, test, out outVar);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(rtest);
+            Console.WriteLine(test);
+            Console.WriteLine(outVar);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        static Boolean TryRefTest(Int32 a, ref Int32 b, ref Test rtest, Test test, out Int32 outVar)
+        {
+            a = 5;
+            b = 5;
+            rtest = new Test() { x = a, y = b };
+            test = rtest;
+            outVar = 10;
+            return true;
+        }
+    }
+}
