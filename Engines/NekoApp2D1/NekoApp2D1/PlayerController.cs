fc1be314ac25e6d8749b0374498c892d56145392
Animeska34 <59974154+Animeska34@users.noreply.github.com>
2022-04-01 21:16:31 +0300

    Update

---
+++ Engines/NekoApp2D1/NekoApp2D1/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using NekoEngine;
+using NekoEngine.Assets;
+using NekoEngine.Components;
+using NekoEngine.Render.OpenGL;
+using System.Numerics;
+
+namespace NekoApp2D1
+{
+    public class PlayerController : Component
+    {
+        SpriteSheetAnimation ani;
+        Single speed = 1.5f;
+        protected override void Setup()
+        {
+            AddComponent<SpriteRenderer>();
+            ani = new SpriteSheetAnimation(new Texture2D("C3ZwL.png", Texture2D.MagFilter.Nearest));
+            ani.framerate = 8;
+            AddComponent<Animator>().animation = ani;
+            Input.map.Add("up", VirtualKey.W);
+            Input.map.Add("down", VirtualKey.S);
+            Input.map.Add("left", VirtualKey.A);
+            Input.map.Add("right", VirtualKey.D);
+        }
+        protected override void Update()
+        {
+            ani.playing = false;
+            if (Input.GetButton("left"))
+            {
+                transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+                ani.SetAnimation(1);
+                ani.playing = true;
+            }
+            if (Input.GetButton("right"))
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+                ani.SetAnimation(3);
+                ani.playing = true;
+            }
+            if (Input.GetButton("up"))
+            {
+                transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+                if (!ani.playing)
+                {
+                    ani.SetAnimation(0);
+                    ani.playing = true;
+                }
+            }
+            if (Input.GetButton("down"))
+            {
+                transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+                if (!ani.playing)
+                {
+                    ani.SetAnimation(2);
+                    ani.playing = true;
+                }
+            }
+            if (!ani.playing)
+                ani.SetFrame(0);
+            else if (ani.frame == 0)
+                ani.SetFrame(1);
+        }
+    }
+}
---
+++ Engines/NekoApp2D1/NekoApp2D1/Program.cs
@@ -0,0 +1,34 @@
+﻿using NekoEngine;
+using NekoEngine.Assets;
+using NekoEngine.Components;
+using NekoEngine.Extensions;
+using System.Numerics;
+
+namespace NekoApp2D1
+{
+    public class Program : NekoProgram
+    {
+        private static void Main()
+        {
+            //Starting NekoEngine
+            new Program();
+        }
+        public override void OnLoad()
+        {
+            //Change window title
+            Neko.title = AppDomain.CurrentDomain.FriendlyName;
+            //Creating scene and attaching camera
+            Scene scene = new Scene();
+            var c = scene.root.Instantiate(new Vector3(0, 0, -10)).AddComponent<Camera>();
+            c.SetMain();
+            c.orthographic = true;
+            //Write your logic here
+            var player = scene.root.Instantiate();
+            player.AddComponent<PlayerController>();
+            player.transform.localScale = new Vector3(2f, 2f, 0);
+
+            scene.Start();
+
+        }
+    }
+}
