eaf8079d1de2c971d34d8da475ce3db2bacc2fd7
Animeska34 <59974154+Animeska34@users.noreply.github.com>
2022-04-04 12:11:47 +0300

    NekoApp2D1 Update

--- Engines/NekoApp2D1/NekoApp2D1/PlayerController.cs
+++ Engines/NekoApp2D1/NekoApp2D1/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using NekoEngine;
 using NekoEngine.Assets;
 using NekoEngine.Components;
+using NekoEngine.Physics;
 using NekoEngine.Render.OpenGL;
 using System.Numerics;
 
@@ -9,15 +10,20 @@ namespace NekoApp2D1
     public class PlayerController : Component
     {
         SpriteSheetAnimation ani;
-        Single speed = 1.5f;
+        Single speed = 2f;
+        bool onGround;
+        bool jumping;
         protected override void Setup()
         {
             AddComponent<SpriteRenderer>();
             ani = new SpriteSheetAnimation(new Texture2D("C3ZwL.png", Texture2D.MagFilter.Nearest));
             ani.framerate = 8;
             AddComponent<Animator>().animation = ani;
+            AddComponent<BoxCollder2D>().size = new Vector2(0.1f, 2f);
+            AddComponent<Rigidbody2D>();
+            Camera.Main.transform.SetParent(transform);
+
             Input.map.Add("up", VirtualKey.W);
-            Input.map.Add("down", VirtualKey.S);
             Input.map.Add("left", VirtualKey.A);
             Input.map.Add("right", VirtualKey.D);
         }
@@ -36,28 +42,37 @@ namespace NekoApp2D1
                 ani.SetAnimation(3);
                 ani.playing = true;
             }
-            if (Input.GetButton("up"))
+            if (Input.GetButtonDown("up") && onGround)
             {
-                transform.position += new Vector3(0, speed * Time.deltaTime, 0);
-                if (!ani.playing)
-                {
-                    ani.SetAnimation(0);
-                    ani.playing = true;
-                }
+                jumping = true;
             }
-            if (Input.GetButton("down"))
+            if (Input.GetButtonUp("up"))
             {
-                transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
-                if (!ani.playing)
-                {
-                    ani.SetAnimation(2);
-                    ani.playing = true;
-                }
+                jumping = false;
             }
+            if (jumping)
+            {
+                transform.position += new Vector3(0, speed * 3 * Time.deltaTime, 0);
+            }
+
+            if (transform.position.Y < -10)
+                transform.position = Vector3.Zero;
+
             if (!ani.playing)
                 ani.SetFrame(0);
             else if (ani.frame == 0)
                 ani.SetFrame(1);
+        }
+
+        protected override void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.tag == "ground")
+                onGround = true;
+        }
+        protected override void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.tag == "ground")
+                onGround = false;
         }
     }
 }
--- Engines/NekoApp2D1/NekoApp2D1/Program.cs
+++ Engines/NekoApp2D1/NekoApp2D1/Program.cs
@@ -13,12 +13,13 @@ namespace NekoApp2D1
             //Starting NekoEngine
             new Program();
         }
+        Scene scene;
         public override void OnLoad()
         {
             //Change window title
             Neko.title = AppDomain.CurrentDomain.FriendlyName;
             //Creating scene and attaching camera
-            Scene scene = new Scene();
+            scene = new Scene();
             var c = scene.root.Instantiate(new Vector3(0, 0, -10)).AddComponent<Camera>();
             c.SetMain();
             c.orthographic = true;
@@ -27,8 +28,27 @@ namespace NekoApp2D1
             player.AddComponent<PlayerController>();
             player.transform.localScale = new Vector3(2f, 2f, 0);
 
+            InstantiateCyclePlatform(new Vector3(0, -3, 1));
+            InstantiatePlatform(new Vector3(5, -4, 1));
             scene.Start();
 
+        }
+
+        void InstantiatePlatform(Vector3 pos)
+        {
+            var tmp = scene.root.Instantiate();
+            tmp.transform.position = pos;
+            tmp.AddComponent<SpriteRenderer>().sprite = new Sprite("Platform.png");
+            tmp.AddComponent<BoxCollder2D>();
+            tmp.tag = "ground";
+        }
+        void InstantiateCyclePlatform(Vector3 pos)
+        {
+            var tmp = scene.root.Instantiate();
+            tmp.transform.position = pos;
+            tmp.AddComponent<SpriteRenderer>().sprite = new Sprite("CyclePlatform.png");
+            tmp.AddComponent<CycleCollder2D>().radius = 1.1f;
+            tmp.tag = "ground";
         }
     }
 }
