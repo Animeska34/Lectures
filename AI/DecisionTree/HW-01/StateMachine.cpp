fb1d284783ada45ebea636d86de2a04782e7f8bb
Animeska34 <59974154+Animeska34@users.noreply.github.com>
2022-10-26 21:56:18 +0300

    Add HW-01

---
+++ AI/DecisionTree/HW-01/StateMachine.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "StateMachine.h"
+#if __has_include("StateMachine.g.cpp")
+#include "StateMachine.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::HW01::implementation
+{
+    StateMachine::StateMachine()
+    {
+        InitializeComponent();
+    }
+
+    int32_t StateMachine::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void StateMachine::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void StateMachine::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
---
+++ AI/DecisionTree/HW-01/StateMachine.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "StateMachine.g.h"
+
+namespace winrt::HW01::implementation
+{
+    struct StateMachine : StateMachineT<StateMachine>
+    {
+        StateMachine();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::HW01::factory_implementation
+{
+    struct StateMachine : StateMachineT<StateMachine, implementation::StateMachine>
+    {
+    };
+}
